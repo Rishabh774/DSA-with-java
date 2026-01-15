1c484bda4391612a0ab3240b64138696f52f4104
Rishabh Sahu <sahurishabh5765@gmail.com>
2026-01-15 12:25:07 +0530

    Bit manipulation

---
+++ Bit_Manipulation/Compliments.java
@@ -0,0 +1,10 @@
+package Bit_Manipulation;
+
+public class Compliments {
+    public static void main(String[] args) {
+        int x = 5;
+        System.out.println(~x); // one's compliment
+        System.out.println(~x+1); // two's compliment
+        System.out.println(-x);// teo's compliment
+    }
+}
---
+++ Bit_Manipulation/SwapNumbers.java
@@ -0,0 +1,21 @@
+package Bit_Manipulation;
+
+import java.util.Scanner;
+
+public class SwapNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Numbers before swapping :");
+        System.out.print("Enter first number :");
+        int num1= sc.nextInt();
+        System.out.print("Enter second  number :");
+        int num2 = sc.nextInt();
+        num1 =num1^num2;
+        num2= num1^num2;
+        num1= num1^num2;
+        System.out.println("After swapping ");
+        System.out.println("First number :"+ num1);
+        System.out.print("Second number :"+ num2);
+
+    }
+}
