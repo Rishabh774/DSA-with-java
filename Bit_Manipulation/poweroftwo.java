b86111930a8b076fbd413140e2397c558b00e921
Rishabh Sahu <sahurishabh5765@gmail.com>
2026-01-15 12:54:53 +0530

    Bit manipulation

---
+++ Bit_Manipulation/poweroftwo.java
@@ -0,0 +1,12 @@
+package Bit_Manipulation;
+
+import java.util.Scanner;
+
+public class poweroftwo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextLong();
+        long x=1;
+        System.out.println(x<<n);
+    }
+}
